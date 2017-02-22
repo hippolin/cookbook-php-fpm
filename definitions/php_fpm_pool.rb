67cb3c49dfc700ee2f6ee63b5f2b554cbf68933b
Hippo <hippo@hippo.idv.tw>
2017-02-22 23:26:07 +0800

    add status_path & ping_path

--- definitions/php_fpm_pool.rb
+++ definitions/php_fpm_pool.rb
@@ -51,6 +51,8 @@ define :php_fpm_pool, :template => "pool.conf.erb", :enable => true do
         :access_log => params[:access_log] || false,
         :php_options => params[:php_options] || {},
         :request_terminate_timeout => params[:request_terminate_timeout],
+        :pm_status_path => params[:pm_status_path],
+        :ping_path => params[:ping_path],
         :params => params
       )
       notifies :restart, "service[php-fpm]"
